e0347f7fd2080f2ef4d6b58c32d6e8747755fc17
Faisal Qureshi <fzubair@gmail.com>
2022-02-17 21:35:15 -0500

    Added train and evaluate files.

---
+++ src/neural-implicit-representation/evaluate.py
@@ -0,0 +1,85 @@
+import sys
+sys.path.append('../')
+
+import numpy as np
+import argparse
+import torch
+import model
+import positional_encoding as pe
+
+def reconstruct_image(model):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(f'Using {device}')
+    model.to(device)
+
+    h, w, pos_enc_dim = model.h, model.w, model.pos_enc_dim
+    
+    output_image = np.empty((h,w))
+    x, y = np.arange(w), np.arange(h)
+    xx, yy = np.meshgrid(x, y)
+    pos = np.stack([xx, yy], axis=2).reshape(-1, 2)
+
+    model.eval()
+    for i in range(pos.shape[0]):
+        x, y = pos[i, 0], pos[i, 1]
+        enc_x = pe.positional_encoding(x, pos_enc_dim)
+        enc_y = pe.positional_encoding(y, pos_enc_dim)
+        inputs = torch.Tensor(np.hstack((enc_x, enc_y))).unsqueeze(0).to(device)
+        c = model(inputs)
+        output_image[int(y)][int(x)] = c.detach().cpu()[0][0][0].numpy()
+    
+    return output_image
+
+def resample_image(model, new_h, new_w):
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(f'Using {device}')
+    model.to(device)
+
+    h, w, pos_enc_dim = model.h, model.w, model.pos_enc_dim
+    
+    output_image = np.empty((new_h,new_w))
+    x, y = np.linspace(0, w, new_w), np.linspace(0, h, new_h)    
+    xx, yy = np.meshgrid(x, y)
+    xx_, yy_ = np.meshgrid(np.arange(new_w), np.arange(new_h))
+    pos = np.stack([xx, yy, xx_, yy_], axis=2).reshape(-1, 4)
+
+    model.eval()
+    for i in range(pos.shape[0]):
+        x, y, x_, y_ = pos[i, 0], pos[i, 1], int(pos[i, 2]), int(pos[i, 3])
+        enc_x = pe.positional_encoding(x, pos_enc_dim)
+        enc_y = pe.positional_encoding(y, pos_enc_dim)
+        inputs = torch.Tensor(np.hstack((enc_x, enc_y))).unsqueeze(0).to(device)
+        c = model(inputs)        
+        output_image[y_][x_] = c.detach().cpu()[0][0][0].numpy()
+    
+    return output_image   
+
+def load_model(filepath):
+    # model -- important that his model matches the checkpoint
+    # file     
+    h, w = 28, 28
+    pos_enc_dim = 8
+    net = model.Nir(h, w, pos_enc_dim, 1)
+
+    if filepath:
+        ckpt = torch.load(filepath)
+        net.load_state_dict(ckpt['net'])
+        loss_best = ckpt['loss']
+        epochs_completed = ckpt['epochs_completed']
+
+        print('Using checkpoint:')
+        print(f'  Epochs completed = {epochs_completed}, loss = {loss_best}')  
+
+    return net
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Neural implicit representation demo')
+    parser = argparse.add_argument('-f', '--file', action='store', dafault=None, help='Checkpoint file')
+    parser = argparse.add_argument('-u', '--upsample', action='store_true', default=False, help='Upsample if specified. Otherwise reconstruct the mnist digit.')
+    args = parser.parse_args()
+
+    model = load_model(args.file)
+    if args.upsample:
+        resample_image(model, 128, 128)
+    else:
+        reconstruct_image(model)
--- src/neural-implicit-representation/train.py
+++ src/neural-implicit-representation/train.py
@@ -17,43 +17,55 @@ def pick_mnist_image(idx=0, root='../data', download=False):
     image = np.array(data[idx][0])
     return image
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Neural implicit representation demo')
-    parser.add_argument('--mnist-id', action='store', default=0, type=int, help='MNIST id of the image used for training')
-    parser.add_argument('--data-dir', action='store', default='.', help='Directory containing data')
-    parser.add_argument('--pos-enc-dim', action='store', type=int, default=8, help='Positional encoding dimenstion')
-    parser.add_argument('--batchsize', action='store', type=int, default=16, help='Specify batchsize for training')
-    parser.add_argument('--shuffle', action='store_true', default=False, help='Set shuffle to true.')
-    parser.add_argument('--epochs', action='store', default=1, type=int, help='Number of epochs')
-
-
-    args = parser.parse_args()
-
-    image = pick_mnist_image(args.mnist_id, root=args.data_dir, download=False)
+def train(mnist_id, data_dir, num_epochs, resume, verbose):
+    # image
+    image = pick_mnist_image(mnist_id, root=data_dir, download=False)
     h, w = image.shape
 
-    plt.figure()
-    plt.title(f'Input image. {h}x{w}. Press q to continue')
-    plt.imshow(image, cmap='gray')
-    plt.show()
-
-    pixels_dataset = pix.PixelsDataset(image=image, pos_enc_dim=args.pos_enc_dim, scale=1./255.) 
+    # construct pixel dataset and dataloader, encoding_dimension
+    pos_enc_dim = 8
+    batch_size = 1024
+    shuffle = True
+    pixels_dataset = pix.PixelsDataset(image=image, pos_enc_dim=pos_enc_dim, scale=1./255.) 
     print(f'Number of pixels = {len(pixels_dataset)}')
 
-    training_dataloader = DataLoader(pixels_dataset, batch_size=args.batchsize, shuffle=args.shuffle)
+    training_dataloader = DataLoader(pixels_dataset, batch_size=batch_size, shuffle=shuffle)
 
+    # compute device
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     print(f'Using {device}')
 
-    nn = model.Nir(h, w, args.pos_enc_dim, 1)
+    # model -- encoding dimension = 16, output 1     
+    net = model.Nir(h, w, pos_enc_dim, 1)
 
+    # load previously saved checkpoint if resuming otherwise start anew
+    if resume:
+        ckpt = torch.load('./nir.pth')
+        net.load_state_dict(ckpt['net'])
+        loss_best = ckpt['loss']
+        epochs_completed = ckpt['epochs_completed']
+
+        print('Resuming from saved checkpoint:')
+        print(f'  Epochs completed = {epochs_completed}, loss = {loss_best}')
+    else:
+        loss_best = -1
+        epochs_completed = 0
+
+    # setting up tensorboard
     writer = SummaryWriter()
 
-    nn.to(device)
+    # push model to compute device
+    # set up loss and optimizer
+    net.to(device)
     loss_fn = torch.nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    for epoch in range(args.epochs):
+    # Set up training loop
+    print_every = 1 if verbose else 10
+
+    epoch = 0
+    while epoch < num_epochs:
+        epoch += 1
         loss_epoch = 0.0
         cnts = 0
         for _, data in enumerate(training_dataloader):
@@ -61,7 +73,7 @@ if __name__ == '__main__':
             labels = data['c'].to(device)
 
             optimizer.zero_grad()
-            outputs = model(inputs)    
+            outputs = net(inputs)    
             loss = loss_fn(outputs, labels)
             loss.backward()
             optimizer.step()
@@ -70,11 +82,40 @@ if __name__ == '__main__':
             loss_epoch += loss.cpu().item()
             
         loss_epoch /= cnts
-        writer.add_scalar('Loss/train', loss, epoch)
+        writer.add_scalar('Loss/train', loss, epoch+epochs_completed)
             
-        if epoch % 1000 == 0 or epoch == num_epochs-1:
-            print(f'epoch = {epoch}: loss = {loss_epoch}')
+        if epoch == 1 or epoch % print_every == 0 or epoch == num_epochs-1:
+            print(f'epoch = {epoch+epochs_completed}: loss = {loss_epoch}')
             
-    #torch.save(model.state_dict(), './nir.pts')
+        if loss_best < 0 or loss_best > loss_epoch:
+            torch.save(
+                {
+                    'net': net.state_dict(),
+                    'loss': loss_epoch,
+                    'epochs_completed': epoch+epochs_completed
+                },
+                './nir.pth'
+            )
+            loss_best = loss_epoch
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Neural implicit representation demo')
+    parser.add_argument('mnistid', action='store', type=int, help='MNIST id of the image used for training')
+    parser.add_argument('--data-dir', action='store', default='.', help='Directory containing data')
+    parser.add_argument('--num_epochs', action='store', default=1, type=int, help='Number of epochs')
+    parser.add_argument('-r', '--resume', action='store_true', default=False, help='Resume training')
+    parser.add_argument('-v', '--verbose', action='store_true', default=False, help='Print loss at every epoch')
+
+    # for argparse it is best to not use das
+    
+    args = parser.parse_args()
+
+    # plt.figure()
+    # plt.title(f'Input image. {h}x{w}. Press q to continue')
+    # plt.imshow(image, cmap='gray')
+    # plt.show()
+
+    train(args.mnistid, args.data_dir, args.num_epochs, args.resume, args.verbose)
+
 
 
